520fa0680d95531ea321e35b89c562e6a21124c2
Montsetec <47403218+Montsetec@users.noreply.github.com>
2019-03-06 15:14:37 -0800

    EJERCICIO DE PAR E IMPAR

---
+++ bloque1/practica1-9.py
@@ -0,0 +1,12 @@
+print ("indique si el numero es par o impar")    
+    
+a = int(input("ingrese un numero\n"))
+if (a%2 == 0): 
+    print (str(a) + " es par")
+else: 
+    print (str(a) + " es impar")
+    
+    
+    
+    # TREVINO MENDEZ MONTSERRAT 
+    # INGENIERIA QUIMICA 
