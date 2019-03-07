7cd7e3d6ded16d3bfb114728b35b58387d49a3c9
elmejorRW <47403044+elmejorRW@users.noreply.github.com>
2019-03-07 14:22:30 -0800

    ejercicio 11

---
+++ bloque1/practica1-11.py
@@ -0,0 +1,21 @@
+print("introducir un conjunto de 25 numeros")
+print("enteros, determinar cuantos son\npositivos y cuantos negativos")
+print("se debe ingresar un numero mayor al -25")
+
+n=25
+x=0
+y=0
+
+dato=int(input("ingrese un numero natural mayor a -25:"))
+while dato +1 <n:
+   if dato > 0:
+      x + 1
+   elif dato < 0:
+      y + 1
+      
+   if dato > 0:
+      x + 1
+   elif dato < 0:
+      y + 1
+ print("la cantidad de numeros positivos es" x,
+ "\nLa cantidad de numeros negativos es", y)
