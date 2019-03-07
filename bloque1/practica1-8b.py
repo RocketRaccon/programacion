84fdee6bb37fabb85896fbac4d7b58a21eac53f7
kenyamoza <47014461+kenyamoza@users.noreply.github.com>
2019-03-07 14:26:20 -0800

    Rename practica1-8.py to practica1-8b.py

---
+++ bloque1/practica1-8b.py
@@ -0,0 +1,20 @@
+#Morales Zalazar Kenya Guadalupe
+
+print("Ejercicio 8. Determinar si un entero dado es un multiplo de 6")
+print("x=12")
+print("y=6")
+print("x%y")
+print(12%6)
+print("si el resultado es 0, entonces si es un multiplo")
+
+print("ejemplo 2")
+print("x=36")
+print("y=6")
+print("x%y")
+print(36%6)
+
+print("ejemplo: cuando NO es multiplo")
+print("x=45")
+print("y=6")
+print("x%y")
+print(45%6)
