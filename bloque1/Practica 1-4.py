b8278bf9e9f2bf9dca867b1649e40d93aab25f8a
Gamaliel <47436036+vc19@users.noreply.github.com>
2019-03-07 15:42:57 -0800

    Create Practica 1-4.py

---
+++ bloque1/Practica 1-4.py
@@ -0,0 +1,10 @@
+print(4, ' Imprimir los enteros del 1 al 30 ,apareados con sus reciprocos')
+
+i = 1
+while i<=30:
+    print(i)
+    i = i + 1
+    print(1/i)
+
+print('listo')
+print('Hecho por Cuevas Verdugo Gamaliel')
