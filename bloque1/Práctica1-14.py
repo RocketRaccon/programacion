1f2ceb8dca9438eca915e77705a478d4d0419692
nairito22 <47014439+nairito22@users.noreply.github.com>
2019-03-10 11:24:09 -0700

    Determinar cual cantidad es mayor: 3**75 o 2**100
    (Este es el que si corre)

---
+++ bloque1/Práctica1-14.py
@@ -0,0 +1,10 @@
+print("ejercicio 14")
+print("Determinar cual cantidad es mayor: 3**75 o 2**100")
+n1=("3**75")
+n2=("2**100")
+if n1>n2:
+    print("El numero mayor es el primer numero. Numero:", n1)
+    
+    
+Por: Gonzalez Villegas Nairobi Candelaria
+Fecha: 7 de marzo del 2019
