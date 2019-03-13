fda77b42b437a8057518229798f17ac65361533a
Elit1 <47403035+Elit1@users.noreply.github.com>
2019-03-12 22:05:14 -0700

    Update and rename README.md to Posible numerador de 14.py

---
+++ bloque1/Posible numerador de 14.py
@@ -0,0 +1,10 @@
+print("Programa 7, posible numerador de 14")
+print("Fuerte Reyes Jesus Emmanuel")
+
+xValor1 = 14
+yValor2 = int(input("ingrese el número"))
+
+if (xValor1 / yValor2) == 0:
+print(yValor2, "es divisor de", xValor1)
+else:
+print(yValor2, "no es divisor de", xValor1)
