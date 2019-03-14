76b0c7a0bd9cbcddfde4aeffade37c9a5f329ce7
SidneyAlarrazabal <47403310+SidneyAlarrazabal@users.noreply.github.com>
2019-03-14 15:12:54 -0700

    Update Practica1-1.py

--- bloque1/Practica1-1.py
+++ bloque1/Practica1-1.py
@@ -1,5 +1,5 @@
-Por:ALARRAZABAL TOLEDANO SIDNEY
-Fecha:06 de Marzo del 2019
+""" Por:ALARRAZABAL TOLEDANO SIDNEY
+Fecha:06 de Marzo del 2019 """
 
 print ("imprimir los enteros del 9 al 43")
 tupla = tuple(range(9, 44))
