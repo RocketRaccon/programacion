f5e6cb0bde8b7012e0f347add8db8c61a27b2526
René Solis <IoTeacher@users.noreply.github.com>
2019-03-14 14:43:43 -0700

    ERRROR

--- bloque1/Actividad1-14.py
+++
@@ -1,9 +0,0 @@
-print("ejercicio 14")
-print("Determinar cual cantidad es mayor: 3**75 o 2**100")
-n1=float(input("3**75"))
-n2=float(input("2**100"))
-if n1>n2:
-    print("El numero mayor es el primer numero. Numero:", n1)
-    
-    Por: Gonzalez Villegas Nairobi Candelaria
-    7 de marzo del 2019
