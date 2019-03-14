bcfee684e776b7aa622dfad8c41f9d9629f11fd5
nairito22 <47014439+nairito22@users.noreply.github.com>
2019-03-14 14:55:00 -0700

    Update Práctica1-14.py

--- bloque1/Práctica1-14.py
+++ bloque1/Práctica1-14.py
@@ -1,10 +1,10 @@
+""" Por: Gonzalez Villegas Nairobi Candelaria """
+""" Fecha: 7 de marzo del 2019 """
+
+
 print("ejercicio 14")
 print("Determinar cual cantidad es mayor: 3**75 o 2**100")
 n1=("3**75")
 n2=("2**100")
 if n1>n2:
     print("El numero mayor es el primer numero. Numero:", n1)
-    
-    
-Por: Gonzalez Villegas Nairobi Candelaria
-Fecha: 7 de marzo del 2019
