ac67089cd6862eff18e8f7668b0d19e0ffb865b5
EstebanEduardo <47403036+EstebanEduardo@users.noreply.github.com>
2019-03-14 15:02:30 -0700

    Rename Capitulo 1-2.py to Capitul0 1-2.py

--- bloque1/Capitul0 1-2.py
+++ bloque1/Capitul0 1-2.py
@@ -5,6 +5,6 @@ def sumalista(listaNumeros):
     laSuma = 0
     for i in listaNumeros:
         laSuma = laSuma + i
-    return laSuma
+    return laSumaEI
 
 print(sumalista([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
