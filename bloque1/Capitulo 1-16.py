b617f97dd6e58cc170838c4110facfc193c058d2
JGL12 <47403053+JGL12@users.noreply.github.com>
2019-03-14 15:04:20 -0700

    Create Capitulo 1-16.py

---
+++ bloque1/Capitulo 1-16.py
@@ -0,0 +1,11 @@
+"""Gomez de Leon Jose Manuel"""
+"""Capitulo 1, Ejercicio 16"""
+
+
+numero =int(input("Ingrese el numero de la tabla de multiplicar: \n"))
+print ("\n")
+print ("Tabla de multiplicar\n")
+rango=range(1,13)
+for elemento in rango:
+    producto=numero*elemento
+    print (numero,'x',elemento,'=',producto)
