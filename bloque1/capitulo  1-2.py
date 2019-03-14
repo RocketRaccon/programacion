e2be853a98a0a2dbe31311eb38f2a04a95afafa3
d23ll <47403070+d23ll@users.noreply.github.com>
2019-03-14 15:01:34 -0700

    practica 6, convertir pulgadas a yardas

---
+++ bloque1/capitulo  1-2.py
@@ -0,0 +1,15 @@
+"""
+print ("practica 6, convertir pulgadas a yardas, y pies a pulgadas")
+print ("Hecho por: Lobatos Leon Dora Luz")
+print ("VII-III-MMXIX")
+"""
+
+xPulgadas=36
+yYardas=float(input("ingrese la cantidad de yardas a convertir"))
+Pulgadas=yYardas*xPulgadas
+print (Pulgadas, "pulgadas equivalen a", yYardas,"yardas")
+
+xPies=12
+yPulgadas=float(input("ingrese la cantidad de Pulgadas a convertir"))
+pies=yPulgadas*xPies
+print (pies, "pies equivalente a", yPulgadas, "Pulgas")
