7cc1fc1da3df20fe20c525f0b2f36e4f866b432c
SidneyAlarrazabal <47403310+SidneyAlarrazabal@users.noreply.github.com>
2019-04-04 15:16:18 -0700

    Create Practica1ATS.py

---
+++ PracticasAbril/Practica1ATS.py
@@ -0,0 +1,10 @@
+#ºF = (ºC · 1,8) + 32
+#Alarrazabal Toledano Sidney
+#Convertir temperaturas Celsius a Fahrenheit
+
+
+print  ("Convertir temperaturas Celsius a Fahrenheit.")
+
+grados1 = int(input('Grados Celsius: '))
+fahrenheit = 9.0/5.0 * grados1 +32
+print('{} grados a Fahrenheit son {}  '.format(grados1, fahrenheit))
