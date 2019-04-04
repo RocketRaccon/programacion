37f9c41ce10044769035653e2438e55766b5b648
l16mc <47403357+l16mc@users.noreply.github.com>
2019-04-04 14:47:05 -0700

    Problema 7

---
+++ Practica 2-7.py
@@ -0,0 +1,3 @@
+celsius = int(input('Ingrese la temperatura en grados Celsius: '))
+fahrenheit = 9.0/5.0 * celsius +32
+print('{} grados Fahrenheit son {} grados Celsius'.format(fahrenheit, celsius))
