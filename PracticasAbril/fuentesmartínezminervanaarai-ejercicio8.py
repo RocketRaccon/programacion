5268612eb4e80f15f9e275d05e7e5e10e5d1bf35
naarai29 <47014479+naarai29@users.noreply.github.com>
2019-04-04 15:41:52 -0700

    Create fuentesmartínezminervanaarai-ejercicio8.py

---
+++ PracticasAbril/fuentesmartínezminervanaarai-ejercicio8.py
@@ -0,0 +1,6 @@
+""" Por: Fuentes Martínez, Minerva Naarai """
+""" Hecho: 04/abril/19 """ 
+
+fahrenheit = int(input('Ingrese una temperatura en grados Fahrenheit: '))
+celsius = (fahrenheit - 32) * 5.0/9.0
+print('{} grados Fahrenheit son {} grados Celsius '.format(fahrenheit, celsius))
