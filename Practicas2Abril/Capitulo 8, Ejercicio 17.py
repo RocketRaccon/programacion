43619ae176ac840af04e64cdc6bd2af4ba9adb1a
Rene Reyes <rene@Renes-MacBook-Pro-2.local>
2019-04-16 21:28:05 -0700

    Ebooks

---
+++ Practicas2Abril/Capitulo 8, Ejercicio 17.py
@@ -0,0 +1,24 @@
+
+print("Gomez de Leon Jose Manuel")
+print("Ing.Quimica")
+print("4/Abril/2019")
+print("\n")
+print("Capitulo 3, Ejercicio 17. Temperatura en farenheit en funcion de los chirridos de un grillo en donde n es el numero de chirridos")
+print("\n")
+t2=(40+40)/4
+t3=(50+40)/4
+t4=(60+40)/4
+t5=(70+40)/4
+t6=(140+40)/4
+t7=(150+40)/4
+print("La temperatura en grados farenheit para n=40 es:", t2)
+print("\n")
+print("La temperatura en grados farenheit para n=50 es:", t3)
+print("\n")
+print("La temperatura en grados farenheit para n=60 es:", t4)
+print("\n")
+print("La temperatura en grados farenheit para n=70 es:", t5)
+print("\n")
+print("La temperatura en grados farenheit para n=140 es:", t6)
+print("\n")
+print("La temperatura en grados farenheit para n=150 es:", t7)
---
+++ Practicas2Abril/orozco-temperatura.py
@@ -0,0 +1,3 @@
+c = int(input('ingrese la cantidad de grados celsius a convertr : '))
+F = 9 / 5 * c + 32 
+print(F)
