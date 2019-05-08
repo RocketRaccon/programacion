9c9b30968369cacf6ae8263431d12b53db799ad7
SidneyAlarrazabal <47403310+SidneyAlarrazabal@users.noreply.github.com>
2019-05-08 15:44:18 -0700

    Add files via upload

---
+++ RepasoMayo/Practicas de Jupyter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Practicas de Jupyther Notebook
+# ## Por Sidney Alarrazabal
+# # Recordando MarkDown
+# 
+# ![imagen](https://i.pinimg.com/originals/d0/59/cf/d059cff89f24a562179a23b6c08b9f4f.jpg)
+
+# In[ ]:
+
+
+## Celsius a Farenheit
+
+celsius = int(input('Ingrese la temperatura en grados Celsius: '))
+fahrenheit = 9.0/5.0 * celsius +32
+print('{} grados Celsius son {} grados Fahrenheit'.format(fahrenheit, celsius))
+
+
+# In[ ]:
+
+
+
+
